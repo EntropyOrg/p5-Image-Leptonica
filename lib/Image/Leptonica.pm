f3ee7b4c6a56f56b3f40e332a217026997798883
Zakariyya Mughal <zaki.mughal@gmail.com>
2013-12-25 21:02:23 -0600

    use XS

--- lib/Image/Leptonica.pm
+++ lib/Image/Leptonica.pm
@@ -3,4 +3,36 @@ package Image::Leptonica;
 use strict;
 use warnings;
 
+use XSLoader;
+XSLoader::load( 'Image::Leptonica', $Image::Leptonica::VERSION );
+
+=method new
+
+Returns a new instance of L<Image::Leptonica>.
+
+=cut
+sub new {
+	bless {}, shift;
+}
+
 1;
+# ABSTRACT: image processing library (using the Leptonica library)
+
+=pod
+
+=head1 SYNOPSIS
+
+  use Image::Leptonica;
+
+  # TODO
+
+=head1 DESCRIPTION
+
+TODO
+
+
+=head1 SEE ALSO
+
+L<Leptonica|http://www.leptonica.com/>
+
+=cut
