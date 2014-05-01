e28db61e297733bd58e00df586637bfbc1323156
Zakariyya Mughal <zaki.mughal@gmail.com>
2014-04-30 22:45:58 -0500

    wip

--- lib/Image/Leptonica.pm
+++ lib/Image/Leptonica.pm
@@ -11,14 +11,18 @@ use Inline;
 
 our $alien = Alien::Leptonica->new;
 
-Inline->bind( C => 'DATA' =>
-        NAME => 'Image::Leptonica' =>
-        VERSION => $Image::Leptonica::VERSION =>
-        INC => $alien->cflags, LIBS => $alien->libs =>
-	ENABLE => AUTOWRAP =>
-        AUTO_INCLUDE => '#include "allheaders.h"');
+my $leptonica_h = file(__FILE__)->dir
+	->file('leptonica.h')
+	->slurp();
 
-1; # [END] (C code goes below this)
+Inline->bind( C => $leptonica_h =>
+	NAME => 'Image::Leptonica' =>
+	VERSION => $Image::Leptonica::VERSION =>
+	INC => $alien->cflags, LIBS => $alien->libs =>
+	ENABLE => AUTOWRAP =>
+	AUTO_INCLUDE => '#include "allheaders.h"');
+
+1;
 
 =pod
 
