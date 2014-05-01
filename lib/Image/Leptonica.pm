ba97a853fc26f5bacb25352f365a0952324e68a8
Zakariyya Mughal <zaki.mughal@gmail.com>
2014-05-01 17:50:53 -0500

    Build results of 4b2fe52 (on master)

--- lib/Image/Leptonica.pm
+++ lib/Image/Leptonica.pm
@@ -65,7 +65,7 @@ Zakariyya Mughal <zmughal@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2013 by Zakariyya Mughal.
+This software is copyright (c) 2014 by Zakariyya Mughal.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
